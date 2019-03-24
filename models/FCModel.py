0e70395df6021623aad53b5590353e2edf6acca7
Gary <carysam@outlook.com>
2019-03-24 10:38:53 +0800

    fc test

--- models/FCModel.py
+++ models/FCModel.py
@@ -78,8 +78,8 @@ class FCModel(CaptionModel):
     def init_hidden(self, bsz):
         weight = next(self.parameters()).data
         if self.rnn_type == 'lstm':
-            return (Variable(weight.new(self.num_layers, bsz, self.rnn_size).zero_()),
-                    Variable(weight.new(self.num_layers, bsz, self.rnn_size).zero_()))
+            return (weight.new(self.num_layers, bsz, self.rnn_size).zero_(),
+                    weight.new(self.num_layers, bsz, self.rnn_size).zero_())
         else:
             return Variable(weight.new(self.num_layers, bsz, self.rnn_size).zero_())
 
